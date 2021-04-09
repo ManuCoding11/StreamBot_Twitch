4711bc689677bf27904d9e3c29db71a23778bf6f
ManuCoding11 <Manuel@event-light-sound.de>
2021-04-09 19:50:18 +0200

    Add channel variable

--- src/commands/game.js
+++ src/commands/game.js
@@ -8,9 +8,10 @@ const obj =
         fetch(`https://decapi.me/twitch/game/${target.slice(1).toLowerCase()}`)
             .then(res => res.text())
             .then(body => {
+                let channel = target[1].toUpperCase() + target.slice(2);
                 let text = (msg.split(' ')[0].toLowerCase() === "!category" || msg.split(' ')[0].toLowerCase() === "!kategorie") ?
-                    `@${context['display-name']} ${target[1].toUpperCase() + target.slice(2)} streamt derzeit in der Kategorie ${body}.` :
-                    `@${context['display-name']} ${target[1].toUpperCase() + target.slice(2)} spielt zurzeit ${body}.`;
+                    `@${context['display-name']} ${channel} streamt derzeit in der Kategorie ${body}.` :
+                    `@${context['display-name']} ${channel} spielt zurzeit ${body}.`;
                 //client.say(target, text);
                 console.log(text);
             })
