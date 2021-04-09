d3e5c1b05b885e5b20abdee9420e1bb8d448fac6
ManuCoding11 <Manuel@event-light-sound.de>
2021-04-09 20:09:19 +0200

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -10,17 +10,19 @@ const opts = {
 		username: process.env.bot_username,
 		password: process.env.bot_password
 	},
-	channels: ["spielbaerlp"]//process.env.channels.split(',')
+	channels: process.env.channels.split(',')
 }
 
-const client = new tmi.client(opts)
+const client = new tmi.client(opts);
 
+client
 	.on('connected', (addr, port) => console.log(`* Connected to ${addr}:${port}, Channels: [${process.env.channels}]`))
 	.on('message', (target, context, msg, self) => {
 
 		if (self) return;
 
-		handler.checkCommand(client, target, context, "!game", self);
+		handler.checkCommand(client, target, context, msg);
+		
 	})
 
 
