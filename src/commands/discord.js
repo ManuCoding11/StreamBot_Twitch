9b71fdac0546c59c03ecd2710d5ec7a60338bbe7
ManuCoding11 <Manuel@event-light-sound.de>
2021-04-10 12:59:29 +0200

    Create discord.js

---
+++ src/commands/discord.js
@@ -0,0 +1,14 @@
+const obj =
+{
+    names: ["!dc", "!discord", "!dcserver"],
+    exec: function (client, target, context, msg, self) {
+        if (target === "#tefanoro") {
+            client.say(target, `@${context['display-name']} https://discord.gg/GER4CT6HWT`);
+        }
+        else if (target === "#bene_1") {
+            client.say(target, `@${context['display-name']} https://discord.gg/VcU4YSpte7`);
+        }
+    }
+}
+
+module.exports.obj = obj;
