771ef4ec2250059e5c3f6e8a4bc67832a0734558
ManuCoding11 <Manuel@event-light-sound.de>
2021-04-19 08:59:41 +0200

    Added case match

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -7,12 +7,16 @@ const obj =
         let text = '';
 
         if (!isNaN(params[0])) {
-            if (Math.floor(n) < 2) text = 'Der Würfelbereich muss größer als 1 sein.';
-            else if (Math.floor(n) > 511) text = 'Der maximale Würfelwert ist 511.';
-            else {
-                let num = Math.floor(Math.random() * Math.floor(n)) + 1;
+            if (Math.floor(params[0]) < 2) text = 'Der Würfelbereich muss größer als 1 sein.';
+            else if (Math.floor(params[0]) > 511) text = 'Der maximale Würfelwert ist 511.';
+            else if (Math.floor(params[0] <= 511 && Math.floor(params[0]) >= 2)) {
+                let num = Math.floor(Math.random() * Math.floor(params[0])) + 1;
                 text = `Du hast eine ${num} gewürfelt.`;
             }
+        }
+        else {
+            let num = Math.floor(Math.random() * 6) + 1;
+            text = `Du hast eine ${num} gewürfelt.`;
         }
 
         console.log(text + "\n");
